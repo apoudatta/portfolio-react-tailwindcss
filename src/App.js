1599e2eed779920e31037a351d0a3362929d9229
apoudatta <apoudatto6@gmail.com>
2024-09-06 14:28:37 +0600

    about done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navber from './Components/Navbar/Navber';
 import Hero from './Components/Hero/Hero';
+import About from './Components/About/About';
 
 const App = props => {
   return (
     <div>
       <Navber />
       <Hero />
+      <About />
     </div>
   );
 };
