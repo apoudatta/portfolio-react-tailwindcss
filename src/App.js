a18bc34295a1d8ac15bfdb1f56ff18a29de4247f
apoudatta <apoudatto6@gmail.com>
2024-09-06 16:05:31 +0600

    services done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navber from './Components/Navbar/Navber';
 import Hero from './Components/Hero/Hero';
 import About from './Components/About/About';
+import Services from './Components/Services/Services';
 
 const App = props => {
   return (
@@ -9,6 +10,7 @@ const App = props => {
       <Navber />
       <Hero />
       <About />
+      <Services />
     </div>
   );
 };
